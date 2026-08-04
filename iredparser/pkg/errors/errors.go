da762d19a91aaf352ee6d7bedc4116f763863f6f
Andrey <ninzyago@gmail.com>
2026-08-04 14:26:37 +0300

    feat(go:errors): separate authentication errors

--- iredparser/pkg/errors/errors.go
+++ iredparser/pkg/errors/errors.go
@@ -12,5 +12,9 @@ var (
 	ErrFailedCaptureCookie  = errors.New("could not capture cookie")
 	ErrInvalidResponseData  = errors.New("invalid response data")
 	ErrInvalidMemorySuffix  = errors.New("invalid memory size suffix")
-	ErrInvalidCredentials   = errors.New("invalid credentials")
+
+	// Authentication errors
+	ErrLoginRequired        = errors.New("login required")
+	ErrInvalidUsername      = errors.New("username must be an valid email address")
+	ErrIncorrectCredentials = errors.New("username or password is incorrect")
 )
