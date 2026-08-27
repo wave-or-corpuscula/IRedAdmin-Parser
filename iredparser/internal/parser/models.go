fe0c9e8509192f2ebec08ad0735d4d51773ee6ec
Andrey <ninzyago@gmail.com>
2026-08-27 11:44:36 +0300

    feat(go:parsing): results models for Domains and Mailboxes parsing

--- iredparser/internal/parser/models.go
+++ iredparser/internal/parser/models.go
@@ -10,6 +10,7 @@ type Domain struct {
 	DisplayName     string `db:"display_name"`
 	QuotaBytes      int64  `db:"quota_bytes"`
 	UsedMemoryBytes int64  `db:"used_memory_bytes"`
+	UsersAmount     int
 }
 
 type Mailbox struct {
@@ -19,4 +20,22 @@ type Mailbox struct {
 	Address         string `db:"address"`
 	QuotaBytes      int64  `db:"quota_bytes"`
 	UsedMemoryBytes int64  `db:"used_memory_bytes"`
+}
+
+type ParseDomainResult struct {
+	Domains []*Domain
+	Total   int
+	Errors  []error
+}
+
+type ParseMailboxesResult struct {
+	Mailboxes []*Mailbox
+	Total     int
+	Errors    []error
+}
+
+func (r *ParseMailboxesResult) Extend(other *ParseMailboxesResult) {
+	r.Mailboxes = append(r.Mailboxes, other.Mailboxes...)
+	r.Total += other.Total
+	r.Errors = append(r.Errors, other.Errors...)
 }
