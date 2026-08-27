e6979d9e07a484b2f287194d3ac41ed937861516
Andrey <ninzyago@gmail.com>
2026-08-27 16:34:25 +0300

    test(go:parsing): unit tests for mailbox parsing (start)

--- iredparser/internal/parser/mailbox/mailbox_parser_test.go
+++ iredparser/internal/parser/mailbox/mailbox_parser_test.go
@@ -2,71 +2,20 @@ package mailboxparser
 
 import (
 	"bytes"
-	"errors"
 	"html/template"
 	"io"
 	"iredparser/internal/parser"
 	"iredparser/internal/parser/client"
 	apperrors "iredparser/pkg/errors"
+	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-const mailboxTemplate = `
-<table>
-	<tbody>
-		{{ range . }}
-		<tr class="{{ if .Disabled }}disabled{{ else }}active{{ end }}">
-			<td class="checkbox">
-					<input type="checkbox" name="mail" class="checkbox" value="{{ .Addres }}" alt="active">
-			</td>
-			<td class="vcenter">
-					<a href="/iredadmin/profile/user/general/{{ .Addres }}">
-							<i class="fa fa-cog fa-lg fr-space"></i>
-					</a>
-					<a href="/iredadmin/profile/user/general/{{ .Addres }}">{{ .DisplayName }}</a>
-			</td>
-			<td class="vcenter">
-					<span><strong>{{ .DisplayName }}</strong></span>
-					<span class="color-grey"><em>{{ .Domain }}</em></span>
-			</td>
-			<td class="vcenter"></td>
-			<td class="vcenter"></td>
-			<td class="vcenter" data-sort-value="0">{{ .Quota }}</td>
-		</tr>
-	</tbody>
-</table>
-`
-
-type MailboxData struct {
-	Disabled    bool
-	IsAdmin     bool
-	DisplayName string
-	Addres      string
-	Domain      string
-	Quota       string
-}
-
-func makeMailbox(disabled bool, isAdmin bool, displayName string, addres string, quota string) (*MailboxData, error) {
-	if !strings.Contains(addres, "@") {
-		return nil, errors.New("no '@' in mailbox addres")
-	}
-
-	domain := strings.Split(addres, "@")[1]
-
-	return &MailboxData{
-		Disabled:    disabled,
-		IsAdmin:     isAdmin,
-		DisplayName: displayName,
-		Addres:      addres,
-		Domain:      domain,
-		Quota:       quota,
-	}, nil
-}
 
 type mockTransport struct {
 	roundTripFunc func(req *http.Request) (*http.Response, error)
@@ -213,6 +162,138 @@ func TestGetPagesAmount(t *testing.T) {
 
 			if tt.expectedError == nil {
 				assert.Equal(t, gotPages, len(tt.pages))
+			}
+		})
+	}
+}
+
+const mailboxTemplate = `
+<table>
+	<tbody>
+		{{ range . }}
+		<tr class="{{ if .Disabled }}disabled{{ else }}active{{ end }}">
+			<td class="checkbox">
+					<input type="checkbox" name="mail" class="checkbox" value="{{ .Addres }}" alt="active">
+			</td>
+			<td class="vcenter">
+					<a href="/iredadmin/profile/user/general/{{ .Addres }}">
+							<i class="fa fa-cog fa-lg fr-space"></i>
+					</a>
+					<a href="/iredadmin/profile/user/general/{{ .Addres }}">{{ .DisplayName }}</a>
+			</td>
+			<td class="vcenter">
+					<span><strong>{{ .DisplayName }}</strong></span>
+					<span class="color-grey"><em>@{{ .Domain }}</em></span>
+			</td>
+			<td class="vcenter"></td>
+			<td class="vcenter"></td>
+			<td class="vcenter" data-sort-value="0">{{ .Quota }}</td>
+		</tr>
+	 {{ end }}
+	</tbody>
+</table>
+`
+
+type MailboxData struct {
+	Disabled    bool
+	IsAdmin     bool
+	DisplayName string
+	Addres      string
+	Domain      string
+	Quota       string
+}
+
+func makeMailbox(disabled bool, isAdmin bool, addres string, displayName string, quota string) *MailboxData {
+	if !strings.Contains(addres, "@") {
+		log.Fatalln("no '@' in mailbox addres")
+	}
+
+	domain := strings.Split(addres, "@")[1]
+
+	return &MailboxData{
+		Disabled:    disabled,
+		IsAdmin:     isAdmin,
+		DisplayName: displayName,
+		Addres:      addres,
+		Domain:      domain,
+		Quota:       quota,
+	}
+}
+
+func makeMailboxesTable(pages int, mailboxes ...*MailboxData) string {
+	mailTmpl, err := template.New("mailboxes").Parse(mailboxTemplate)
+	if err != nil {
+		log.Fatalf("cannot compile mailbox template: %v", err)
+	}
+
+	var mailboxesTable bytes.Buffer
+	err = mailTmpl.Execute(&mailboxesTable, mailboxes)
+	if err != nil {
+		log.Fatalf("cannot execute template: %v", err)
+	}
+
+	pagesTmpl, err := template.New("pages").Parse(pagesTemplate)
+	if err != nil {
+		log.Fatalf("cannot compile pages template: %v", err)
+	}
+
+	pageValues := make([]string, pages)
+	for i := range pages {
+		pageValues[i] = strconv.Itoa(i + 1)
+	}
+
+	var pagesFooter bytes.Buffer
+	err = pagesTmpl.Execute(&pagesFooter, pageValues)
+	if err != nil {
+		log.Fatalf("cannot execute pages template: %v", err)
+	}
+
+	mailboxPage := mailboxesTable.String() + pagesFooter.String()
+
+	return mailboxPage
+}
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name          string
+		pages         int
+		mailboxes     []*MailboxData
+		workers       int
+		expectedError error
+	}{
+		{
+			name:  "success - valid mailboxes",
+			pages: 1,
+			mailboxes: []*MailboxData{
+				makeMailbox(false, false, "test1@domain.com", "test1", "0 Bytes / Unlimited"),
+				makeMailbox(false, false, "test2@domain.com", "test2", "2 GB / 4 GB"),
+				makeMailbox(false, false, "test3@domain.com", "test2", "345 KB / 2 GB"),
+				makeMailbox(false, false, "test4@domain.com", "test4", "1 MB / Unlimited"),
+			},
+			workers:       1,
+			expectedError: nil,
+		}, // TODO: Добавить кейсов
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := func(req *http.Request) (*http.Response, error) {
+				if req.Method != http.MethodGet {
+					t.Fatalf("expected GET method, got %v", req.Method)
+				}
+
+				table := makeMailboxesTable(tt.pages, tt.mailboxes...)
+				resp := makeResponse(http.StatusOK, table, nil)
+
+				return resp, nil
+			}
+
+			mailboxParser := getTestMailboxParser(handler, tt.workers)
+			result, err := mailboxParser.Parse(t.Context(), "testmailserver", parser.Domain{Name: "test.com"})
+			assert.ErrorIs(t, err, tt.expectedError)
+
+			if tt.expectedError == nil {
+				assert.Len(t, result.Mailboxes, len(tt.mailboxes))
 			}
 		})
 	}
